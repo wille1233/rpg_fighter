30d9b0b5f21d4531ca6c612677b4fbcd26d1fb58
wille1233 <officiellawilliam@gmail.com>
2022-02-16 15:08:57 +0100

    Updated version

--- src/main.py
+++ src/main.py
@@ -1,4 +1,68 @@
 import resources
+from resources import Character, Goblin
+from random import randint, shuffle, choice
+
+def fight(players : list, enemies : list):
+    participants = players + enemies # Create initiative order
+    shuffle(participants)
+
+    for char in participants:
+        target = ""
+        # Is the character a player or a goblin?
+        if char in players:
+            target = choice(enemies)
+        else:
+            target = choice(players)
+
+        target.take_damage(char.get_attack())
+        if target.get_health() == 0:
+            print(f"{target.get_name()} has died!")
+            if type(target) == Goblin:
+                enemies.remove(target)
+            else:
+                players.remove(target)
+            participants.remove(target)
+        
+        else:
+            print(f"{target.get_name()} has {target.get_health()} health remaining.")
+
+        if len(enemies) == 0 or len(players) == 0: # If there is no enemies or players left.
+            break #dance
+
+
+
 
 if __name__ == "__main__":
-    pass
+
+    enemies = []
+    players = []
+
+    nemy = Character("Emy", 20, 5, 2)
+    nick = Character("Nick", 15, 2, 1)
+    players.append(nemy)
+    players.append(nick)
+
+    enemies.append(Goblin(10, 3, 2, 1))
+    enemies.append(Goblin(15, 2, 1, 2))
+    enemies.append(Goblin(12, 3, 1, 3))
+
+    round = 1
+    while len(enemies) != 0 and len(players) != 0:
+        print(f"\nROUND {round}, FIGHT!")
+        fight(players, enemies)
+        round +=1
+    
+    if len(enemies) == 0:
+        print("The Players won!")
+        for x in players:
+            print(f"The surviors:\n {x}")
+
+    elif len(players) == 0:
+        print("The Goblins won!")
+        for x in enemies:
+            print(f"The surviors:\n {x}")
+
+    
+
+    
+    
--- src/resources.py
+++ src/resources.py
@@ -1,10 +1,56 @@
 class Character:
+    
     def __init__(self, name, health, attack, armor):
         self.name = name
         self.health = health
         self.attack = attack
         self.armor = armor
-
+        
     def __str__(self) -> str:
         return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+    
+    
+    def get_attack(self): # Earlier damage
+        return self.attack
+    
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        if relative_damage > 0:
+            self.health -= relative_damage
+        if self.health < 0: self.health = 0
+
+    def get_health(self):
+        return self.health
+
+    def get_name(self):
+        return self.name
+
+class Goblin:
+    
+    def __init__(self, health, attack, armor, id):
+        self.health = health
+        self.attack = attack
+        self.armor = armor
+        self.id = id 
         
+    def __str__(self) -> str:
+        return f"Goblin #{self.id}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+
+
+    
+    def take_damage(self, damage):
+        relative_damage = damage - self.armor
+        if relative_damage > 0:
+            self.health -= relative_damage
+        if self.health < 0: self.health = 0
+
+
+    def get_health(self):
+        return self.health
+    
+    def get_attack(self):
+        return self.attack
+
+    def get_name(self):
+        return f"Goblin #{self.id}"
+    
